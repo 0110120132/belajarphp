fbc690fa4bfdd948b8b080926967771237dbcfb3
0110120132 <danielalka234@gmail.com>
2021-02-25 15:28:23 +0700

    belajar php dasar

---
+++ hello.php
@@ -0,0 +1,13 @@
+<?php
+
+$nama = "daniel adi saputra";
+$kelas = "SI04";
+$nim = 0110120132;
+
+echo "nama :" . $nama .;
+echo "kelas :" . $kelas .;
+echo "nim:" . $nim .;
+echo '<br>';
+echo "hello word";
+
+echo "<h1>heloo word<h1/>";
